f816f428174fd769b43f9cc754525d8b4a19d5c6
rick <ricklikeswaffles@gmail.com>
2024-10-03 10:56:26 -0700

    init

---
+++ initial-installs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt update
+
+# Ansible
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+
+# SSH Server
+sudo apt install openssh-server
+sudo systemctl enable ssh
+
+# to transfer private key:
+# scp <oldmachine>:/home/rick/.ssh/* ~/.ssh/
